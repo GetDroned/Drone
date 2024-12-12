5aafcb31c469a8183d303b8538d1073e63ce9192
SaraMattiato-coder <sara.mattiato@gmail.com>
2024-12-12 12:26:22 +0100

    Fixed structure

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+// lib.rs
+
+pub mod get_droned;
+pub use get_droned::{init_logger, GetDroned};
--- src/main.rs
+++
@@ -1,5 +0,0 @@
-pub mod get_droned;
-
-fn main() {
-    println!("Thank you for using Get Droned !");
-}
